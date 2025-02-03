40d7db51128c78e43b28d293afcb2f428e10b8a7
jair1c <gabrieljair.gc@gmail.com>
2025-02-03 14:02:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,6 @@ onload = () => {
             "Hace solo un mes comenzamos esta historia oficialmente, pero siento que mi corazón te ha pertenecido desde siempre. Gracias por hacerme tan feliz, María. ¡Por muchos meses más juntos!",
             "Un mes a tu lado ha sido suficiente para darme cuenta de que quiero muchos más contigo. Eres mi alegría y mi razón de sonreír. ¡Feliz primer mes, mi amor!",
             "Solo ha pasado un mes, pero cada día contigo es un recuerdo hermoso. Gracias por ser mi novia, mi compañía y mi felicidad. ¡Te adoro, Mi Mujercita!",
-            "Nuestro primer mes juntos es solo el inicio de una historia maravillosa. No puedo esperar para seguir construyendo momentos inolvidables a tu lado. ¡TE QUIEROOO!",
             "Dicen que lo bueno pasa rápido, y este mes a tu lado ha volado. Gracias por cada sonrisa y cada momento especial. ¡Feliz primer mes, mi María hermosa!",
             "Dicen que la distancia separa cuerpos, pero nunca corazones. Cada día confirmo que lo nuestro es real, fuerte y hermoso. ¡Feliz primer mes, María, mi amor eterno!",
             "Este es solo el primer capítulo de nuestra historia, y aunque hoy no podamos abrazarnos, sé que un día la distancia será solo un recuerdo. ¡Te quiero con todo mi corazón!"
